f6d9944373bb4306e66088dc240a63953ed78b6a
shibano <shibano@tmi.t.u-tokyo.ac.jp>
2024-11-07 17:54:36 +0900

    接続先エンドポイントとコントラクトアドレスを外部化．

--- src/blockchain/contractService.js
+++ src/blockchain/contractService.js
@@ -1,8 +1,8 @@
 import { ethers } from "ethers";
 import contractData from "@/blockchain/abi/Counter.json";
 
-const provider = new ethers.JsonRpcProvider("http://localhost:8545");
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const provider = new ethers.JsonRpcProvider(process.env.VUE_APP_PROVIDER_URL);
+const contractAddress = process.env.VUE_APP_CONTRACT_ADDRESS;
 const contractABI = contractData.abi;
 
 const contract = new ethers.Contract(contractAddress, contractABI, provider);
